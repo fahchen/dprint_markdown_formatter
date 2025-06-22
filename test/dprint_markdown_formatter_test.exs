ed67e6910a2c925f77ad94e6619b43f71e08b5bd
Phil Chen <06fahchen@gmail.com>
2025-06-22 13:02:32 +0800

    initial commit

---
+++ test/dprint_markdown_formatter_test.exs
@@ -0,0 +1,8 @@
+defmodule DprintMarkdownFormatterTest do
+  use ExUnit.Case
+  doctest DprintMarkdownFormatter
+
+  test "greets the world" do
+    assert DprintMarkdownFormatter.hello() == :world
+  end
+end
